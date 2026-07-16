300f3dd50c4f3c880a7d4ef86b783dadaf5bd95b
sohelempiricinfotech-sys <sohel.empiricinfotech@expert.micro1.ai>
2026-07-16 14:21:38 +0530

    Create integrations.js

---
+++ src/integrations.js
@@ -0,0 +1,11 @@
+const AWS = require('aws-sdk');
+const { shopifyApi, LATEST_API_VERSION } = require('@shopify/shopify-api');
+const ExampleCRM = require('@examplecrm/sdk');
+
+// AWS appears in source and config: inventory it as one vendor integration.
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const shopify = shopifyApi({ apiVersion: '2025-10' });
+const googlePlacesLegacyUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+const exampleCrm = new ExampleCRM.Client({ region: 'us' });
+module.exports = { s3, sqs, shopify, googlePlacesLegacyUrl, exampleCrm, LATEST_API_VERSION };
